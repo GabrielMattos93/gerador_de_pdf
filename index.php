68b5ef8252dc0132b01a3acb122e036b7bc306b3
GabrielMattos93 <71332438+GabrielMattos93@users.noreply.github.com>
2022-08-23 20:16:16 -0300

    Gerando o Arquivo PDF
    Gerando arquivo em PDF (Retrato ou Paisagem).

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+
+    "<title>Gerador de PDF</title>
+      <h1>Gerador de PDF</h1>"; 
+
+require 'vendor/autoload.php';
+
+// Referenciar o dompdf com namespace
+use Dompdf\Dompdf; 
+
+
+//Instanciando a classe dompdf
+
+$gerador_de_pdf = new Dompdf();
+
+//Instanciando o método e loadHtml e enviar o contéudo do PDF
+$gerador_de_pdf->loadHtml('Olá Mundo - Este é o Gerador de PDF');
+
+// Orientação da folha do arquivo (Paisagem).
+//$gerador_de_pdf->setPaper('A4', 'landscape');
+
+$gerador_de_pdf->setPaper('A4', 'portrait'); 
+
+// Renderizar o Html como PDF
+$gerador_de_pdf->render();
+
+// Output the generated PDF to Browser
+$gerador_de_pdf->stream();
